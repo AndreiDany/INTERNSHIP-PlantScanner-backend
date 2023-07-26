6c7185e502d16610312f6d55b8aea0f9fcb3b291
AndreiDany~ <daniel.andrei@emanuel.ro>
2023-07-26 15:02:46 +0300

    rezolvarea erorii cauzata de CSRF

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/verify-pin', [App\Http\Controllers\API\PinController::class, 'verifyPin']);
+
 Route::get('/', function () {
     return view('welcome');
 });
